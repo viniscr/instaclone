35c87ba56257ac995aedd99dcee8a17a2db51727
Vinicius Barbosa <viniciusbarbosa@Viniciuss-MacBook-Pro.local>
2019-11-05 15:40:38 -0300

    First commit

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Feed from './Pages/Feed';
+
+import logo from './assets/instagram.png'
+
+const Routes = createAppContainer(
+    createStackNavigator({
+        Feed,
+     }, {
+            defaultNavigationOptions:{
+                headerTitle: <Image source={logo} />,
+                headerStyle: {
+                    backgroundColor: '#f5f5f5'
+                },
+            }
+    })
+);
+
+export default Routes;
